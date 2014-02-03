80d0eb4f96e551d168d38dfa92655a027d23c653
App Academy Student <student@appacademy.io>
2014-02-03 13:53:11 -0800

    added user input, play loop; fixed reveal for 0 case

--- minesweeper.rb
+++ minesweeper.rb
@@ -21,7 +21,7 @@ class MinesweeperTile
     edge_bombs = self.adjacent_bombs
 
     if edge_bombs == 0
-      @game.neighbors.each.reveal
+      @game.neighbors(@pos).each { |tile| tile.reveal }
     end
     nil
   end
@@ -46,6 +46,62 @@ class MinesweeperGame
   attr_reader :grid
   def initialize(bomb_freq = 0.2, x_dim = 9, y_dim = 9)
     @grid = populate_grid(bomb_freq, x_dim, y_dim)
+  end
+
+  def play
+    display_board
+    while true
+      command, x_pos, y_pos = get_user_input
+      x_pos = x_pos.to_i
+      y_pos = y_pos.to_i
+      case command
+      when "r"
+        grid[x_pos][y_pos].reveal
+      when "f"
+        grid[x_pos][y_pos].flagged = true
+      when "u"
+        grid[x_pos][y_pos].flagged = false
+      end
+      #check_victory
+      display_board
+    end
+  end
+
+  def get_user_input
+    valid_input = false
+    inputs = ""
+    until valid_input
+      puts "Please enter the coordinates of the space you wish to affect."
+      puts "Possible actions: f = flag, r = reveal, u = unflag"
+      puts "(Example:  'f, 0, 1')"
+      inputs = gets.chomp.gsub(" ", "").split(",")
+      valid_input = valid_input?(inputs)
+      if !valid_input
+        puts "Error: Input invalid.  Please re-enter."
+      end
+    end
+    inputs
+  end
+
+  def valid_input?(inputs)
+    if !["f", "r", "u"].include?(inputs[0])
+      puts "1"
+      return false
+    elsif !(0..grid.length - 1).include?(inputs[1].to_i)
+      puts "2"
+      return false
+    elsif !(0..grid[0].length - 1).include?(inputs[2].to_i)
+      puts "3"
+      return false
+    elsif grid[inputs[1].to_i][inputs[2].to_i].revealed
+      puts "4"
+      return false
+    elsif inputs[0] == "u" && !grid[inputs[1].to_i][inputs[2].to_i].flagged
+      puts "5"
+      return false
+    else
+      true
+    end
   end
 
   def populate_grid(bomb_freq, x_dim, y_dim)
